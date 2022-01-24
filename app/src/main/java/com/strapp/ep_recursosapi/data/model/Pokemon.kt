b956fcb0c35d856259c6e379288f0fadc19c28e7
Daniel J <48568101+daniel-jaimes@users.noreply.github.com>
2022-01-24 23:16:03 +0100

    add unversioned files

--- app/src/main/java/com/strapp/ep_recursosapi/data/model/Pokemon.kt
+++ app/src/main/java/com/strapp/ep_recursosapi/data/model/Pokemon.kt
@@ -2,9 +2,7 @@ package com.strapp.ep_recursosapi.data.model
 
 class Pokemon {
     var name: String
-        get() = name
     var heigh : Int
-        get() = heigh
 
     constructor(name: String, heigh: Int){
         this.name = name
