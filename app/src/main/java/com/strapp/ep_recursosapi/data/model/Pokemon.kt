310e2e30d2a1276038900b85b80f7ffa9d4abbb4
daniel--jaimes <48568101+daniel-jaimes@users.noreply.github.com>
2022-01-25 17:09:19 +0100

    Delete unused properties in Pokemon.kt

--- app/src/main/java/com/strapp/ep_recursosapi/data/model/Pokemon.kt
+++ app/src/main/java/com/strapp/ep_recursosapi/data/model/Pokemon.kt
@@ -1,23 +1,10 @@
 package com.strapp.ep_recursosapi.data.model
 
 class Pokemon(
-    val id: Int,
     val name: String,
     val height : Int,
-    val is_default: Boolean,
-    val order: Int,
     val weight: Int,
     val abilities: List<Any>,
-    val forms: List<Any>,
-    val game_indices: List<Any>,
-    val held_items: List<Any>,
-    val location_area_encounters: String,
-    val moves: List<Any>,
-    val past_types: List<Any>,
-    val sprites: Any,
-    val species: Any,
-    val stats: List<Any>,
-    val types: List<Any>
 )
 {
 
